9ddc9b125c675c8d380f7d47a0d0feaac111aa4d
Ryan Dancy <ryan@keal.ca>
2016-11-19 20:11:35 -0500

    Remove schema nesting as it's not supported
    For some reason Mongoose doesn't support schema nesting, so now we do
    the nesting with plain objects.

--- models/item.js
+++ models/item.js
@@ -4,15 +4,15 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var reqt = require('./util/req-types.js');
 
 // Helper schema to avoid repetition
-var Character = new Schema({
+var Character = {
   type: reqt.String,
   minlength: 1,
   maxlength: 1,
   required: true
-});
+};
 
 // Control fields -- 00X
-var marcControlField = new Schema({
+var marcControlField = {
   tag: {
     type: reqt.Number,
     min: 0,
@@ -20,17 +20,17 @@ var marcControlField = new Schema({
   },
   value: reqt.String,
   required: true
-});
+};
 
 // Subfields
-var marcSubfield = new Schema({
+var marcSubfield = {
   tag: Character,
   value: reqt.String,
   required: true
-});
+};
 
 // Variable fields
-var marcVariableField = new Schema({
+var marcVariableField = {
   tag: {
     type: reqt.Number,
     min: 10,
@@ -40,7 +40,7 @@ var marcVariableField = new Schema({
   ind1: Character,
   ind2: Character,
   required: true
-});
+};
 
 module.exports = mongoose.model('Item', {
   marc: {
