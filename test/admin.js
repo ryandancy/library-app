e12a818f4c07aa20edd4a105afd4a0968e1a61b7
Ryan Dancy <ryan@keal.ca>
2016-12-04 08:21:04 -0500

    Add tests for /v0/admins sortability
    OMG ALL THE COPY AND PASTING. Anyways, added lots of tests (11 in fact)
    to test the sort_by/direction query parameters on /v0/admins. They all
    pass, but the copy and pasting is surreal.

--- test/admin.js
+++ test/admin.js
@@ -102,8 +102,6 @@ function checkAndSanitizeResponseDoc(doc) {
   doc.should.have.property('created');
   doc.should.have.property('updated');
   new Date(doc.updated).should.be.at.most(new Date(doc.created));
-  delete doc.updated;
-  delete doc.created;
   
   return doc;
 }
@@ -122,6 +120,8 @@ function testGet(path, testDocs, done) {
       
       for (var resDoc of res.body) {
         var doc = checkAndSanitizeResponseDoc(resDoc);
+        delete doc.created;
+        delete doc.updated;
         testDocs.should.deep.include(doc);
       }
       
@@ -156,6 +156,243 @@ describe('Admins', () => {
       getGetTester(path, testAdmins.whitespace));
     it('can retrieve all of them at once',
       getGetTester(path, Object.values(testAdmins)));
+    // TODO cleanup all the copy + pasting
+    it('defaults to sorting ascending by created date', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              new Date(prevAdmin.created).should.be
+                .at.most(new Date(admin.created));
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort descending by created date', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?direction=desc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              new Date(prevAdmin.created).should.be
+                .at.least(new Date(admin.created));
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can explicitly sort by created date, defaulting to ascending', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=created')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              new Date(prevAdmin.created).should.be
+                .at.most(new Date(admin.created));
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can explicitly sort ascending by created date', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=created&direction=asc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              new Date(prevAdmin.created).should.be
+                .at.most(new Date(admin.created));
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can explicitly sort descending by created date', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=created&direction=desc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              new Date(prevAdmin.created).should.be
+                .at.least(new Date(admin.created));
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort ascending by name lexicographically', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=name')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              prevAdmin.name.should.be.at.most(admin.name);
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort explicitly ascending by name lexicographically', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=name&direction=asc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              prevAdmin.name.should.be.at.most(admin.name);
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort descending by name lexicographically', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=name&direction=desc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              prevAdmin.name.should.be.at.least(admin.name);
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort ascending by ID', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=id')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              prevAdmin.id.should.be.below(admin.id);
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort explicitly ascending by ID', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=id&direction=asc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              prevAdmin.id.should.be.below(admin.id);
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
+    it('can sort descending by ID', done => {
+      populateDB(Object.values(testAdmins), testAdmins => {
+        chai.request(server)
+        .get(path + '?sort_by=id&direction=desc')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(testAdmins.length);
+          
+          var prevAdmin = null;
+          for (var admin of res.body) {
+            if (prevAdmin !== null) {
+              prevAdmin.id.should.be.above(admin.id);
+            }
+            prevAdmin = admin;
+          }
+          
+          done();
+        });
+      }, done);
+    });
   });
   describe('POST /v0/admins', () => {
     it('creates a valid admin', done => {
