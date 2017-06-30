b0b74e5e3e4cda40cd4d84f20400f3f5ae04a8d7
Ryan Dancy <ryan@keal.ca>
2017-06-30 16:07:20 -0400

    Allow POST /v0/items with Content-Type=application/marc
    Now you can POST an item using MARC. A barcode is generated and the
    hashing algorithm is absolutely *horrible*.

--- routes/routes.js
+++ routes/routes.js
@@ -112,6 +112,101 @@ module.exports = (router, baseUri) => {
   });
   
   addCollection(Item, 'items', {
+    // Parse MARC and convert to item format
+    create: (req, res, marc, next) => {
+      if (req.get('Content-Type') !== 'application/marc') return next();
+      
+      let jsonMarc = marcConvert.marcToJson(marc);
+      let item = {marc: jsonMarc, status: 'in'};
+      
+      function findSubfield(field, subfield) {
+        for (let subf of field.subfields) {
+          if (subf.tag === subfield) {
+            return subf.value;
+          }
+        }
+      }
+      
+      /*
+        Necessary information is found in the following MARC fields:
+        - 020$a: ISBN
+        - 100$a: author
+        - 245$a: title
+        - 245$b: subtitle
+        - 250$a: edition
+        - 260$a: pubPlace - note: unknown == '[S.1.]'
+        - 260$b: publisher
+        - 260$c: pubYear
+      */
+      
+      // Find all fields, put in `fields`, removing from tagsToFind each time
+      // This makes sure that for repeated fields, the first field is used
+      let tagsToFind = [20, 100, 245, 250, 260];
+      let fields = {};
+      
+      for (let field of jsonMarc.fields.variable) {
+        if (tagsToFind.includes(field.tag)) {
+          fields[field.tag] = field;
+          tagsToFind = tagsToFind.filter(tag => tag !== field.tag);
+        }
+      }
+      
+      // Transfer fields to `item` (dealing with pubPlace specially)
+      
+      let transferData = [ // [field name, tag, subfield, numeric?]
+        ['isbn', 20, 'a', true],
+        ['author', 100, 'a', false],
+        ['title', 245, 'a', false],
+        ['subtitle', 245, 'b', false],
+        ['edition', 250, 'a', false],
+        ['pubPlace', 260, 'a', false],
+        ['publisher', 260, 'b', false],
+        ['pubYear', 260, 'c', true]
+      ];
+      
+      for (let [name, tag, subfield, numeric] of transferData) {
+        if (tag in fields) {
+          let subf = findSubfield(fields[tag], subfield);
+          if (subf !== undefined // for pubPlace, [S.1.] means unknown
+              && !(name === 'pubPlace' && subf === '[S.1.]')) {
+            if (numeric) {
+              // This mess is to try to extract numbers from crap like 'c1991x'
+              for (let i = 0; i < subf.length; i++) {
+                subf = parseInt(subf.slice(i), 10);
+                if (!isNaN(subf)) break;
+              }
+              if (isNaN(subf)) continue;
+            }
+            item[name] = subf;
+          }
+        }
+      }
+      
+      // Generate a barcode -- REVIEW should we even be generating barcodes???
+      // HACK this is possibly the worst hashing algorithm ever devised
+      // So we add up all the things in a string, add up the bytes, multiply
+      // by a prime, add some random kajiggle, modulo by a bunch, and hope it
+      // works, m'kay? What could possibly go wrong?
+      let strToConvert = (item.isbn || '').toString() + (item.author || '')
+        + (item.title || '') + (item.subtitle || '') + (item.edition || '')
+        + (item.pubPlace || '') + (item.publisher || '')
+        + (item.pubYear || '').toString();
+      
+      let barcode = 0;
+      for (let i = 0; i < strToConvert.length; i++) {
+        barcode += strToConvert.charCodeAt(i);
+      }
+      
+      barcode *= 2662816691; // magic numbers are always a good idea, right?
+      barcode += Math.floor(Math.abs(Math.random()) * 1000000);
+      barcode %= 10000000000; // hopefully 10 digits is what we want?
+      barcode = barcode.toString();
+      barcode = '0'.repeat(10 - barcode.length) + barcode; // pad to 10 digits
+      
+      item.barcode = barcode;
+      
+      next(item);
+    },
     delete: (req, res, item, next) => {
       let promises = [];
       
